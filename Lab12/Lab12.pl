f8aed622574b23c930df613a6a5061f71819f2c7
Al Maz <alexmaznitskiy@gmail.com>
2022-05-03 19:42:47 +0300

    added 12.11 (Down)

--- Lab12/Lab12.pl
+++ Lab12/Lab12.pl
@@ -16,3 +16,8 @@ notSimDelSum_Up(X, Res) :- nsds(X, X, Res).
 nsds(_, 2, 1) :- !.
 nsds(X, I, Res) :- I1 is I - 1, nsds(X, I1, Res1), (not(is_Simple(I)), 0 =:= (X mod I), Res is Res1 + I; Res is Res1), !.
 
+% 11 Найти сумму непростых делителей числа (рекурсия вниз)
+notSimDelSum_Down(X, Res) :- nsds_(X, X, 1, Res).
+nsds_(_, 2, Res, Res) :- !.
+nsds_(X, I, CurSum, Res) :- I1 is I - 1, (not(is_Simple(I)), 0 =:= (X mod I), !), NewSum is CurSum + I, nsds_(X, I1, NewSum, Res);
+                         I2 is I - 1, nsds_(X, I2, CurSum, Res).
