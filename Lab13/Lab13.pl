3e82d31905616742971f5adaa015220c0153d6e2
Al Maz <alexmaznitskiy@gmail.com>
2022-05-13 00:09:53 +0300

    added 13.18

--- Lab13/Lab13.pl
+++ Lab13/Lab13.pl
@@ -151,3 +151,22 @@ task17 :-
     next_to([glass, _], [can, _], Drinks),
     next_to([glass, _], [_, milk], Drinks),
     write(Drinks), !.
+
+% 18 
+task18 :- 
+    Professions = [_, _, _, _],
+    in_list(Professions, [voronov, _]),
+    in_list(Professions, [pavlov, _]),
+    in_list(Professions, [levizkiy, _]),
+    in_list(Professions, [saharov, _]),
+    in_list(Professions, [_, dancer]),
+    in_list(Professions, [_, painter]),
+    in_list(Professions, [_, singer]),
+    in_list(Professions, [_, writer]),
+    not(in_list(Professions, [voronov, singer])),
+    not(in_list(Professions, [levizkiy, singer])),
+    not(in_list(Professions, [pavlov, painter])),
+    not(in_list(Professions, [pavlov, writer])),
+    not(in_list(Professions, [saharov, writer])),
+    not(in_list(Professions, [voronov, writer])),
+    write(Professions), !.
