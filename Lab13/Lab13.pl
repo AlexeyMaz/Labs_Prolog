3c512c8767a0f8662de25695daf7ca20237686b7
Al Maz <alexmaznitskiy@gmail.com>
2022-05-12 22:27:07 +0300

    added 13.16

--- Lab13/Lab13.pl
+++ Lab13/Lab13.pl
@@ -97,3 +97,17 @@ task15 :-
     not(in_list(Dress, [valya, _, green])),
     not(in_list(Dress, [natasha, G, G])),
     write(Dress), !.
+
+% 16 [фамилия, профессия, siblings_count, возраст]
+task16 :- 
+    Factory = [_, _, _, _],
+    in_list(Factory, [borisov, _, 1, _]),
+    in_list(Factory, [ivanov, _, _, _]),
+    in_list(Factory, [semenov, _, _, 2]),
+    in_list(Factory, [_, slesar, 0, 0]),
+    in_list(Factory, [_, tokar, _, 1]),
+    in_list(Factory, [_, svarshik, _, _]),
+    in_list(Factory, [Friend1, slesar, _, _]),
+    in_list(Factory, [Friend2, tokar, _, _]),
+    in_list(Factory, [Friend3, svarshik, _, _]),
+    write('slesar is '), write(Friend1), nl, write('tokar is '), write(Friend2), nl, write('svarshick is '), write(Friend3), !.
