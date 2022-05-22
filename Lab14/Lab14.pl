21306b5b020f47928064bf7a3ee13c96c0c5a414
Al Maz <alexmaznitskiy@gmail.com>
2022-05-22 20:29:54 +0300

    added 14.1.3

--- Lab14/Lab14.pl
+++ Lab14/Lab14.pl
@@ -7,14 +7,35 @@ r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Fl
 write_str([]):-!.
 write_str([H|Tail]):-put(H),write_str(Tail).
 
+concatStr([], B, B) :- !.
+concatStr([Head | Tail], X, [Head | T]) :- concatStr(Tail, X, T).
+
+count(List, X, Result) :- count(List, X, 0, Result).
+count([], _, Result, Result) :- !.
+count([X|T], X, CurCnt, Result) :- NewCnt is CurCnt + 1, count(T, X, NewCnt, Result), !.
+count([_|T], X, CurCnt, Result) :- count(T, X, CurCnt, Result), !.
+
 % Задание 1
-% 1.1
+% 1.1 Дана строка. Вывести ее три раза через запятую и показать количество символов в ней.
 task1_1 :- write('Input string: '), read_str(Str, Len), write_str(Str), write(', '), write_str(Str), write(', '), write_str(Str), nl, write('Length: '), write(Len).
 
-% 1.2
+% 1.2 Дана строка. Найти количество слов.
 count_symbols(Str, Search, Result) :- char_code(Search, SCode), count_symbols(Str, SCode, 0, Result).
 count_symbols([], _, Result, Result) :- !.
 count_symbols([S|T], Search, CurKol, Result) :- S = Search, NewKol is CurKol + 1, count_symbols(T, Search, NewKol, Result), !.
 count_symbols([_|T], Search, CurKol, Result) :- count_symbols(T, Search, CurKol, Result), !.
 
 task1_2 :- write('Input string: '), read_str(Str, _), count_symbols(Str, ' ', SpacesKol), WordsKol is SpacesKol + 1, write('Words count: '), write(WordsKol).
+
+% 1.3 Дана строка, определить самое частое слово.
+split_str([], _, CurWord, CurWordList, Result) :- concatStr(CurWordList, [CurWord], NewWL), Result = NewWL, !.
+split_str([Separator|T], Separator, CurWord, CurWordList, Result) :- concatStr(CurWordList, [CurWord], NewWL), split_str(T, Separator, [], NewWL, Result), !.
+split_str([S|T], Separator, CurWord, CurWordList, Result) :- concatStr(CurWord, [S], NewWord), split_str(T, Separator, NewWord, CurWordList, Result), !.
+split_str(Str, Separator, Result) :- char_code(Separator, SepCode), split_str(Str, SepCode, [], [], Result).
+
+most_freq_word(Str, Result) :- split_str(Str, ' ', Words), most_freq_word(Words, Words, 0, [], Result).
+most_freq_word(Words, [Word|T], CurMaxCnt, _, Result) :- count(Words, Word, Cnt), Cnt > CurMaxCnt, NewMax is Cnt, NewMaxWord = Word, most_freq_word(Words, T, NewMax, NewMaxWord, Result), !.
+most_freq_word(Words, [_|T], CurMaxCnt, CurMaxWord, Result) :- most_freq_word(Words, T, CurMaxCnt, CurMaxWord, Result), !.
+most_freq_word(_, [], _, Result, Result) :- !.
+
+task1_3 :- write('Input string: '), read_str(Str, _), most_freq_word(Str, X), write('Most frequent word: '), write_str(X).
